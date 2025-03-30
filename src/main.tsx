dd2b421581acfe40da1eb5f2a735f3c93ffb275e
Gentle_Ego <135257358+Gentle-Ego@users.noreply.github.com>
2025-03-30 19:47:37 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
-
 import { createRoot } from 'react-dom/client'
 import ThemeWrapper from './ThemeWrapper.tsx'
 import './index.css'
+import App from './App.tsx'; // Import the App component
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(<ThemeWrapper />);
